b1cba78afa722137cb6c7b05e02d9d1ddf73a49f
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-09-15 21:30:58 +0200

    DELETE

--- tower/tower/src/make/mod.rs
+++ tower/tower/src/make/mod.rs
@@ -1,9 +1,7 @@
 //! Trait aliases for Services that produce specific types of Responses.
 
-mod make_connection;
 mod make_service;
 
-pub use self::make_connection::MakeConnection;
 pub use self::make_service::shared::Shared;
 pub use self::make_service::{AsService, IntoService, MakeService};
 
